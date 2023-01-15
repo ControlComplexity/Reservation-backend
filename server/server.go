1fb95ddbc571182f87123291a379bd33a93d93e3
wangcao <LVQian280@>
2023-01-15 23:42:32 +0800

    解决跨域问题

--- server/server.go
+++ server/server.go
@@ -2,6 +2,8 @@ package server
 
 import (
 	"context"
+	"fmt"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
@@ -12,8 +14,8 @@ import (
 	"net/http"
 	"reservation/config"
 	"reservation/github.com/reservation/api"
-	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 )
+
 type apiServiceImpl struct {
 	api.UnimplementedReservationServiceServer
 	config config.Config
@@ -22,13 +24,16 @@ type apiServiceImpl struct {
 type Validator interface {
 	Validate() error
 }
+
 func InitLogger() {
 	// 通过 zap.NewProduction() 创建一个 logger
 	logger, _ = zap.NewProduction()
 }
 
 var logger *zap.Logger
+
 func RunGRPCServer(config config.Config) error {
+	fmt.Print("version32")
 	InitLogger()
 	interceptor := func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
 		if p, ok := req.(Validator); ok {
@@ -81,7 +86,6 @@ func RunHTTPServer(config *config.Config) error {
 	conn, err := grpc.DialContext(
 		ctx,
 		"localhost"+config.GRPCPort,
-		grpc.WithBlock(),
 		grpc.WithInsecure(),
 		grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(math.MaxUint32)),
 	)
@@ -93,18 +97,29 @@ func RunHTTPServer(config *config.Config) error {
 	if err != nil {
 		log.Fatal("start http server failed, " + err.Error())
 	}
-	gwServer := &http.Server{
-		Addr:    config.HTTPPort,
-		Handler: gateway,
-	}
-	err = gwServer.ListenAndServe()
+
+	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")                                                                                         // 指明哪些请求源被允许访问资源，值可以为 "*"，"null"，或者单个源地址。
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token")                              //对于预请求来说，指明了哪些头信息可以用于实际的请求中。
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")                                                                       //对于预请求来说，哪些请求方式可以用于实际的请求。
+		w.Header().Set("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type") //对于预请求来说，指明哪些头信息可以安全的暴露给 CORS API 规范的 API
+		w.Header().Set("Access-Control-Allow-Credentials", "true")                                                                                 //指明当请求中省略 creadentials 标识时响应是否暴露。对于预请求来说，它表明实际的请求中可以包含用户凭证。
+		fmt.Println("aaaaaaa")
+		//放行所有OPTIONS方法
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(200)
+			return
+		}
+		gateway.ServeHTTP(w, r)
+	}))
+	err = http.ListenAndServe(config.HTTPPort, nil)
 	if err != nil {
 		log.Fatal("RunHttpServer().start HTTP server failed. " + err.Error())
 		return err
 	}
+
 	return nil
 }
-
 
 // NewReservationServer creates an instance of DataManagerServiceServer
 func NewReservationServer(config config.Config) (api.ReservationServiceServer, error) {
